1935949dd51d271b6b337c863d5d0ce5b0d8cebc
dimon-777 <dimon_85@hotmail.com>
2019-01-23 14:46:50 -0500

    Regex validate PIN code

--- 01_23_19_3.js
+++ 01_23_19_3.js
@@ -11,9 +11,7 @@
 
 function validatePIN (pin) {
     
-    // return /^\d+$/.test(pin) && (pin.length === 4 || pin.length === 6)
-        return /^(\d{4}|\d{6})$/.test(pin)
-      
+    return /^\d+$/.test(pin) && (pin.length === 4 || pin.length === 6)
 }
 
 console.log(validatePIN("1234"));
@@ -21,3 +19,7 @@ console.log(validatePIN("12345"));
 console.log(validatePIN("a234"));
 
  // Best solution in one line of code:
+
+//  function validatePIN(pin) {
+//     return /^(\d{4}|\d{6})$/.test(pin)
+//   }
